c436546011a3ca22ec51407b02bb48132d975d74
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-16 16:05:13 +0700

    chore: MainActivity update

--- app/src/main/java/ru/kheynov/horney/MainActivity.kt
+++ app/src/main/java/ru/kheynov/horney/MainActivity.kt
@@ -3,13 +3,7 @@ package ru.kheynov.horney
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import ru.kheynov.horney.screens.MainScreen
 import ru.kheynov.horney.ui.theme.HorneyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HorneyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HorneyTheme {
-        Greeting("Android")
-    }
-}
+
