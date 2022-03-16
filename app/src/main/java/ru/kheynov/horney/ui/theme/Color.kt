48b61f6cb0167896ce25f8418f3c684a3633dca4
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-16 16:03:58 +0700

    feat: add theming

--- app/src/main/java/ru/kheynov/horney/ui/theme/Color.kt
+++ app/src/main/java/ru/kheynov/horney/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package ru.kheynov.horney.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Violet400 = Color(0xFFa78bfa)
+val Violet500 = Color(0xFFa78bfa)
+
+val LightBackground = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF27272a)
+
--- app/src/main/java/ru/kheynov/horney/ui/theme/Theme.kt
+++ app/src/main/java/ru/kheynov/horney/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Violet400,
+    primaryVariant = Violet500,
+    secondary = Violet500,
+    background = DarkBackground,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Violet500,
+    primaryVariant = Violet500,
+    secondary = Violet500,
+    background = LightBackground,
 
     /* Other default colors to override
     background = Color.White,
