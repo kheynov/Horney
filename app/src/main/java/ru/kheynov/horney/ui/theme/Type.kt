8e8b4541a3da3c551c302c6413ece8c4738113b6
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-16 16:03:16 +0700

    chore: add fonts

--- app/src/main/java/ru/kheynov/horney/ui/theme/Type.kt
+++ app/src/main/java/ru/kheynov/horney/ui/theme/Type.kt
@@ -1,18 +1,54 @@
 package ru.kheynov.horney.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import ru.kheynov.horney.R
+
+private val fredokaOneFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.fredoka_one_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal,
+        ),
+    )
+)
+
+private val openSansFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.opensans_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal,
+        ),
+    )
+)
+private val ralewayFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.raleway_reqular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal,
+        ),
+        Font(
+            resId = R.font.raleway_semibold,
+            weight = FontWeight.SemiBold,
+            style = FontStyle.Normal,
+        ),
+    )
+)
 
 // Set of Material typography styles to start with
+private val defaultTypography = Typography()
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+    body1 = defaultTypography.body1.copy(fontFamily = openSansFontFamily),
+    h1 = defaultTypography.h1.copy(fontFamily = fredokaOneFontFamily),
+    h2 = defaultTypography.h2.copy(fontFamily = ralewayFontFamily),
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
