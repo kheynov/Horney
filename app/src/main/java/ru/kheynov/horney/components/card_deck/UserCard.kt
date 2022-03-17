72bdba3014fe59818a4b230d7aa2e390a39fbbc6
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-17 15:55:07 +0700

    feat: add image loading library

--- app/src/main/java/ru/kheynov/horney/components/card_deck/UserCard.kt
+++ app/src/main/java/ru/kheynov/horney/components/card_deck/UserCard.kt
@@ -6,15 +6,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import ru.kheynov.horney.ui.theme.Violet500
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun UserCard(
@@ -30,12 +33,21 @@ fun UserCard(
         Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    //TODO: Create content in card
-                    text = "Horny again...",
-                    color = Violet500,
-                    style = MaterialTheme.typography.h2,
-                    fontSize = 48.sp,
+                GlideImage(
+                    imageModel = "https://picsum.photos/400/600",
+                    shimmerParams = ShimmerParams(
+                        baseColor = MaterialTheme.colors.surface,
+                        highlightColor = Color.White,
+                        durationMillis = 500,
+                        dropOff = 0.65f,
+                        tilt = 20f,
+                    ),
+                    requestOptions = {
+                        RequestOptions()
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .centerCrop()
+                    },
+                    contentScale = ContentScale.FillBounds
                 )
             }
         }
--- app/src/main/java/ru/kheynov/horney/screens/CardDeck.kt
+++ app/src/main/java/ru/kheynov/horney/screens/CardDeck.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ru.kheynov.horney.components.card_deck.BottomNavigation
 import ru.kheynov.horney.components.card_deck.Header
 import ru.kheynov.horney.components.card_deck.UserCard
 import ru.kheynov.horney.ui.theme.HorneyTheme
@@ -18,7 +17,8 @@ import ru.kheynov.horney.ui.theme.HorneyTheme
 @Composable
 fun CardDeck() {
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .padding(bottom = 55.dp),
         color = MaterialTheme.colors.background
     ) {
         Column(
@@ -26,24 +26,24 @@ fun CardDeck() {
 
             ) {
             Column(Modifier
-                .weight(14f)
+                .weight(20f)
                 .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally) {
                 Header(Modifier
-                    .padding(top = 18.dp)
-                    .weight(1.5f))
+                    .padding(top = 32.dp)
+                    .weight(2f))
                 UserCard(
                     Modifier
                         .fillMaxWidth(0.9f)
-                        .padding(top = 20.dp)
+                        .padding(top = 26.dp)
                         .weight(14f)
                 )
                 Spacer(modifier = Modifier.weight(0.7f))
             }
-            BottomNavigation(modifier = Modifier
-                .weight(2f),
-                elevation = 10.dp
-            )
+//            BottomNavigation(modifier = Modifier
+//                .weight(2f),
+//                elevation = 10.dp
+//            )
         }
     }
 }
