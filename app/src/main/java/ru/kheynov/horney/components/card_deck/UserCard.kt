39039c04980b08a7bf7a2186d3f4d11feabf90d2
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-17 21:56:49 +0700

    chore

--- app/src/main/java/ru/kheynov/horney/components/card_deck/UserCard.kt
+++ app/src/main/java/ru/kheynov/horney/components/card_deck/UserCard.kt
@@ -1,5 +1,6 @@
 package ru.kheynov.horney.components.card_deck
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -20,24 +20,27 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.skydoves.landscapist.ShimmerParams
 import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.rememberDrawablePainter
 
 @Composable
 fun UserCard(
     modifier: Modifier = Modifier,
     elevation: Dp = 10.dp,
-    shape: Shape = RoundedCornerShape(10.dp),
+    cornerRadius: Dp = 10.dp,
 ) {
     Card(
         elevation = elevation,
         modifier = modifier,
-        shape = shape,
+        shape = RoundedCornerShape(cornerRadius * 1.4f),
     ) {
         Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 GlideImage(
-                    modifier = Modifier.padding(5.dp).clip(shape = shape),
-                    imageModel = "https://picsum.photos/400/600",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clip(shape = RoundedCornerShape(cornerRadius)),
+                    imageModel = "https://picsum.photos/800/1000",
                     shimmerParams = ShimmerParams(
                         baseColor = MaterialTheme.colors.surface,
                         highlightColor = Color.White,
