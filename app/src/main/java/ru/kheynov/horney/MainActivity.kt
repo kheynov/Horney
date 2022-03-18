a190c1f33485c3a6b3c91e4f4be93371773aae9c
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-18 18:32:58 +0700

    fix & chore

--- app/src/main/java/ru/kheynov/horney/MainActivity.kt
+++ app/src/main/java/ru/kheynov/horney/MainActivity.kt
@@ -3,17 +3,15 @@ package ru.kheynov.horney
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.BottomAppBar
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.Navigation
 import androidx.navigation.compose.rememberNavController
 import ru.kheynov.horney.components.BottomNavigationBar
-import ru.kheynov.horney.components.card_deck.Header
-import ru.kheynov.horney.screens.CardDeck
 import ru.kheynov.horney.ui.theme.HorneyTheme
 import ru.kheynov.horney.utility.Navigation
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +23,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
-        bottomBar = { BottomNavigationBar(navController = navController)}
+        bottomBar = { BottomNavigationBar(navController = navController) }
     ) {
         Navigation(navController)
     }
