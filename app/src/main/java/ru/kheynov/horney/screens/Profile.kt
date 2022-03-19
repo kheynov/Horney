0e1d56a7824f00c182e96b227fb45b3ca601f9b3
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-19 14:01:58 +0700

    refactor: profile screen refactoring

--- app/src/main/java/ru/kheynov/horney/screens/Profile.kt
+++ app/src/main/java/ru/kheynov/horney/screens/Profile.kt
@@ -3,13 +3,16 @@ package ru.kheynov.horney.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import ru.kheynov.horney.components.common.Header
 
 @Composable
 fun ProfileScreen() {
@@ -17,11 +20,20 @@ fun ProfileScreen() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-            Text(text = "PROFILE WOULD BE THERE",
-                fontSize = 36.sp,
-                style = MaterialTheme.typography.h2)
+        Column(Modifier.fillMaxSize()) {
+            Header()
+
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "PROFILE WOULD BE THERE",
+                    fontSize = 36.sp,
+                    style = MaterialTheme.typography.h2,
+                textAlign = TextAlign.Center)
+            }
         }
     }
 }
