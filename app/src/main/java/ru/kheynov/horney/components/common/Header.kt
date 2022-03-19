7f29487b5531003b3d3f7d118e63943888094d74
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-19 14:00:34 +0700

    refactor: fix header behaviour

--- app/src/main/java/ru/kheynov/horney/components/common/Header.kt
+++ app/src/main/java/ru/kheynov/horney/components/common/Header.kt
@@ -2,12 +2,15 @@ package ru.kheynov.horney.components.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -15,7 +18,9 @@ fun Header(
     modifier: Modifier = Modifier,
     fontSize: TextUnit = 38.sp,
 ) {
-    Row(modifier = modifier,
+    Row(modifier = modifier
+        .padding(top = 16.dp, bottom = 16.dp)
+        .fillMaxWidth(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically) {
         Text(
