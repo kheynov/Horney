57ab2b32cae37d242167c452a0b0c5f570be3862
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-19 14:01:24 +0700

    feat: add like info functionality

--- app/src/main/java/ru/kheynov/horney/components/likes/LikeInfo.kt
+++ app/src/main/java/ru/kheynov/horney/components/likes/LikeInfo.kt
@@ -1,21 +1,83 @@
 package ru.kheynov.horney.components.likes
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import ru.kheynov.horney.ui.theme.HorneyTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.glide.GlideImage
+import entities.ProfileCard
+
 
 @Composable
-fun LikesInfo() {
-    
-}
+fun LikeInfo(
+    modifier: Modifier = Modifier,
+    profileCard: ProfileCard,
+) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(1.dp, color = MaterialTheme.colors.secondary),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Row {
+            GlideImage(
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp)
+                    .clip(CircleShape),
+                imageModel = profileCard.images[0],
+                shimmerParams = ShimmerParams(
+                    baseColor = MaterialTheme.colors.surface,
+                    highlightColor = Color.White,
+                    durationMillis = 500,
+                    dropOff = 0.65f,
+                    tilt = 20f,
+                ),
+                requestOptions = {
+                    RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .centerCrop()
+                },
+                contentScale = ContentScale.FillBounds,
+            )
 
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
-@Composable
-fun LikesInfoPreview() {
-    HorneyTheme {
-        LikesInfo()
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp, top = 5.dp),
+            ) {
+                Text(
+                    text = "${profileCard.name}, ${profileCard.age}",
+                    style = MaterialTheme.typography.h2,
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colors.onBackground
+                )
+
+                Text(
+                    modifier = Modifier.padding(top = 5.dp),
+                    text = "Match from ${profileCard.matchTime}",
+                    style = MaterialTheme.typography.body1,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Justify,
+                    color = MaterialTheme.colors.onBackground
+                )
+            }
+        }
+
     }
 }
--- app/src/main/java/ru/kheynov/horney/screens/Likes.kt
+++ app/src/main/java/ru/kheynov/horney/screens/Likes.kt
@@ -1,27 +1,68 @@
 package ru.kheynov.horney.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import entities.ProfileCard
+import ru.kheynov.horney.components.common.Header
+import ru.kheynov.horney.components.likes.LikeInfo
 
 @Composable
 fun LikesScreen() {
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-            Text(text = "LIKES WOULD BE THERE",
-                fontSize = 36.sp,
-                style = MaterialTheme.typography.h2)
+        Column {
+            Header()
+
+            Column(Modifier.padding(8.dp, bottom = 70.dp)) {
+                val items = mutableListOf(
+                    ProfileCard(images = listOf("https://picsum.photos/800/1000"),
+                        name = "Anna",
+                        age = 12),
+                    ProfileCard(images = listOf("https://picsum.photos/850/1000"),
+                        name = "Maria",
+                        age = 24),
+                    ProfileCard(images = listOf("https://picsum.photos/800/1200"),
+                        name = "Viktoria",
+                        age = 19),
+                    ProfileCard(images = listOf("https://picsum.photos/800/900"),
+                        name = "Vitaliy",
+                        age = 20),
+                    ProfileCard(images = listOf("https://picsum.photos/810/1000"),
+                        name = "Igor'",
+                        age = 21),
+                    ProfileCard(images = listOf("https://picsum.photos/830/1000"),
+                        name = "Samuel",
+                        age = 50),
+                    ProfileCard(images = listOf("https://picsum.photos/870/1200"),
+                        name = "Kyle",
+                        age = 18),
+                    ProfileCard(images = listOf("https://picsum.photos/890/900"),
+                        name = "Smith",
+                        age = 29),
+                )
+                LazyColumn {
+                    items(items.size) { idx ->
+                        LikeInfo(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 12.dp),
+                            profileCard = items[idx]
+                        )
+                    }
+                }
+
+            }
         }
     }
 }
