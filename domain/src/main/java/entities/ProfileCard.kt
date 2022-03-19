4210bf59a58e5167b1f69305ba9842f67ccd9905
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-19 14:02:22 +0700

    feat: extend ProfileCard model

--- domain/src/main/java/entities/ProfileCard.kt
+++ domain/src/main/java/entities/ProfileCard.kt
@@ -1,5 +1,8 @@
 package entities
 
 data class ProfileCard(
-    val images: List<String>
+    val images: List<String>,
+    val name: String = "Null",
+    val age: Int = 0,
+    val matchTime: String = "19/03/2022",
 )
