d9954a5ae51bd5d45bf70c877226ebdb598da033
Vladislav Kheynov <lcastory86@gmail.com>
2022-03-21 09:41:38 +0700

    feat: change cards test data from hardcode to repository

--- app/src/main/java/ru/kheynov/horney/screens/CardDeck.kt
+++ app/src/main/java/ru/kheynov/horney/screens/CardDeck.kt
@@ -10,13 +10,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import entities.ProfileCard
+import ru.kheynov.data.repository.CardsRepositoryImpl
 import ru.kheynov.horney.components.card_deck.ProfileCardsDeck
 import ru.kheynov.horney.components.common.Header
 import ru.kheynov.horney.ui.theme.HorneyTheme
+import use_cases.GetCardsUseCase
 
 
 @Composable
 fun CardDeck() {
+    val cardsRepository = CardsRepositoryImpl()
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -27,56 +31,7 @@ fun CardDeck() {
             .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally) {
             Header()
-            val items = mutableListOf(
-                ProfileCard(
-                    images = listOf("https://picsum.photos/800/1000"),
-                    name = "Anna",
-                    age = 21,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/850/1000"),
-                    name = "Maria",
-                    age = 24,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/800/1200"),
-                    name = "Viktoria",
-                    age = 19,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/800/900"),
-                    name = "Vitaliy",
-                    age = 20,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/810/1000"),
-                    name = "Igor'",
-                    age = 21,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/830/1000"),
-                    name = "Samuel",
-                    age = 50,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/870/1200"),
-                    name = "Kyle",
-                    age = 18,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-                ProfileCard(
-                    images = listOf("https://picsum.photos/890/900"),
-                    name = "Smith",
-                    age = 29,
-                    aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ),
-            )
+            val items = GetCardsUseCase(cardsRepository).invoke()
             ProfileCardsDeck(
                 items = items,
                 modifier = Modifier
